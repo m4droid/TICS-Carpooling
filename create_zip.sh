cf410269e5d520c286f0ed53454e5bf69743d85a
Francisco Madrid <fjmadrid@gmail.com>
2015-05-31 18:12:25 -0300

    script para crear zip

---
+++ create_zip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+zip -r "tarea3 - grupo23.zip" \
+	../tarea3grupo23/frontend \
+	../tarea3grupo23/index.html \
+	../tarea3grupo23/backend \
+	../tarea3grupo23/sql \
+	../tarea3grupo23/README.md \
+	-x "*.DS_Store" 

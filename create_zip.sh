b91363ab4ad90e63edf8ee775567766881a149c6
Francisco Madrid <fjmadrid@gmail.com>
2015-06-08 04:05:38 -0300

    fix del script de entrega

--- create_zip.sh
+++ create_zip.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-zip -r "tarea3 - grupo23.zip" \
-	../tarea3grupo23/bower_components \
-	../tarea3grupo23/frontend \
-	../tarea3grupo23/index.html \
-	../tarea3grupo23/backend \
-	../tarea3grupo23/sql \
-	../tarea3grupo23/README.md \
-	-x "*.DS_Store" 
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR/../" && zip -r "tarea3grupo23/tarea3 - grupo23.zip" tarea3grupo23/bower_components tarea3grupo23/frontend tarea3grupo23/index.html tarea3grupo23/backend tarea3grupo23/sql tarea3grupo23/README.md -x "*.DS_Store"
